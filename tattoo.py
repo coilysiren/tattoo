c6664dc4d07fa3ac04a8ce3a6dbdc98c64da33f0
Lynn Cyrin <firemagelynn@gmail.com>
2018-02-27 16:19:24 -0800

    initial commit

---
+++ tattoo.py
@@ -0,0 +1,24 @@
+from . import (
+    entities, roles
+)
+
+class Lynn(
+    entities.Human,
+):
+    name = 'lynn cyrin'
+    role = roles.SoftwareEngineer
+    sexuality = 'queer'
+    race = 'diasporic african'
+    gender_prefix = 'trans'
+    gender = 'femme'
+
+    def work_output(self):
+        return super()\
+            .work_output() * 2
+
+    @property
+    def jobs(self):
+        return [
+            'NASA',
+            'Bundler',
+            'Callisto',
